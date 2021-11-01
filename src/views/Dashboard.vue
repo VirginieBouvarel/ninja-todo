bc3359daee1d64b78785e8b4fa7761d2e55d268f
Virginie <vb@ikimo9.com>
2021-11-01 14:51:29 +0100

    ajout chip status et btn filtres

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,8 +4,19 @@
 
     <v-container class="my-5">
       
+      <v-row class="ml-3 mb-3">
+        <v-btn small text dark color="grey" @click="sortBy(criteria.title)">
+          <v-icon left small>mdi-folder</v-icon>
+          <span class="text-caption text-lowercase">by project title</span>
+        </v-btn>
+        <v-btn small text dark color="grey" @click="sortBy(criteria.person)">
+          <v-icon left small>mdi-account</v-icon>
+          <span class="text-caption text-lowercase">by person</span>
+        </v-btn>
+      </v-row>
+
       <v-card flat class="pa-3" v-for="project in projects" :key="project.title">
-        <v-row :class="`project ${project.status}`">
+        <v-row :class="`project ${project.status}`" class="align-center">
           <v-col cols="12" md="6">
             <div class="caption grey--text">Project Title</div>
             <div>{{ project.title}}</div>
@@ -19,15 +30,16 @@
            <div>{{ project.due}}</div>
           </v-col>
           <v-col  cols="2" sm="4" md="2">
-            <div class="caption grey--text">Status</div>
-           <div>{{ project.status}}</div>
+            <div class="text-right">
+             <v-chip small :class="`white--text text-caption ${project.status}`">{{ project.status }}</v-chip>
+            </div>
           </v-col>
         </v-row>
 
         <v-row>
           <v-divider></v-divider>
         </v-row>
-        
+
       </v-card>
       
     </v-container>
@@ -40,13 +52,22 @@
   export default {
     name: 'Dashboard',
     data: () => ({
+      criteria: {
+        title: "title",
+        person: "person"
+      },
       projects: [
         { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
-    })
+    }),
+    methods: {
+      sortBy(criteria) {
+        this.projects.sort((a,b) => a[criteria] < b[criteria] ? -1 : 1);
+      }
+    }
   }
 </script>
 
@@ -59,5 +80,14 @@
 }
 .project.overdue {
   border-left: 4px solid tomato;
+}
+.v-chip.complete {
+  background: #3cd1c2 !important;
+}
+.v-chip.ongoing {
+  background: orange !important;
+}
+.v-chip.overdue {
+  background: tomato !important;
 }
 </style>
