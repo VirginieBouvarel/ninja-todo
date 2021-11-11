5e01cf9a016051b02b14e6e6aeaf84890127d2ec
Virginie <vb@ikimo9.com>
2021-11-11 18:05:21 +0100

    ajout dropdown menu dans Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,35 @@
         <span>Ninja</span>
       </v-app-bar-title>
       <v-spacer></v-spacer>
+
+      <!-- dropdown menu -->
+      <v-menu offset-y>
+
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="grey"
+            text
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon left>mdi-chevron-down</v-icon>
+            <span class="text--uppercase grey--text">Menu</span>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="link in links"
+            :key="link.text"
+            :to="link.route"
+          >
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+
+      </v-menu>
+      <!-- end of dropdown menu -->
+
       <v-btn text color="grey">
         <span>Sign Out</span>
         <v-icon class="grey--text" right>mdi-exit-to-app</v-icon>
