d0d8a549844f4296b9672d7140c0972ef15136fd
Virginie <vb@ikimo9.com>
2021-12-07 09:26:13 +0100

    ajout form in popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -18,6 +18,13 @@
         <v-card-title>
           Add a new project
         </v-card-title>
+        <v-card-text>
+          <v-form class="px-3">
+            <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
+            <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil"></v-textarea>
+            <v-btn depressed tile class="success mx-0 mt-3" @click="submit">ADD PROJECT</v-btn>
+          </v-form>
+        </v-card-text>
       </v-card>
 
     </v-dialog>
@@ -26,7 +33,17 @@
 
 <script>
   export default {
-    
+    data() {
+      return {
+        title: '',
+        content:'',
+      }
+    },
+    methods: {
+      submit() {
+        console.log(this.title, this.content);
+      }
+    }
   }
 </script>
 
